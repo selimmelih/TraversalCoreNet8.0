46737aedee475434d7601618c4f0a6878c5ffcc4
selimmelih <selimmelih@outlook.com>
2025-01-15 02:17:07 +0300

    rota detaylarisayfasi eklendi

--- TraversalCoreProje/Controllers/DestinationController.cs
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Controllers
 {
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
         public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+        public IActionResult DestinationDetails(int id)
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination dest)
         {
             return View();
         }
