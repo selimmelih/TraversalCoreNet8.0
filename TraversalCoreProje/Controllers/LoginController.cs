17bd2481104f96330f1d0dd781e0cbfbd42ee7ae
selimmelih <selimmelih@outlook.com>
2025-01-17 01:45:00 +0300

    register process has completed

--- TraversalCoreProje/Controllers/LoginController.cs
+++ TraversalCoreProje/Controllers/LoginController.cs
@@ -1,23 +1,57 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProje.Models;
 
 namespace TraversalCoreProje.Controllers
 {
     [AllowAnonymous]
     public class LoginController : Controller
     {
-        [HttpGet]
+        private readonly UserManager<AppUser> _userManager;
+
+		public LoginController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpGet]
         public IActionResult SignUp()
         {
             return View();
         }
 
 
-        //[HttpPost]
-        //public IActionResult SignUp()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserRegisterViewModel p)
+        {
+            AppUser appUser = new AppUser()
+            {
+               Name = p.Name,
+               Surname = p.Surname,
+               Email =  p.Mail,
+               UserName = p.Username
+            };
+            // şifre hashlenerek gittigi icin bu yapıyı kullanıyoruz başta almıyoruz sifreyi.
+            if (p.Password == p.ConfirmPassword)
+            {
+                var result = await _userManager.CreateAsync(appUser, p.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("SignIn");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+				}
+			}
+			return View(p);
+           
+        }
 
 
         public IActionResult SignIn()
--- TraversalCoreProje/Program.cs
+++ TraversalCoreProje/Program.cs
@@ -1,27 +1,54 @@
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// var app e kadar olan kýsým identity auth ile ilgili önemli bir kýsým.
+builder.Services.AddDbContext<Context>();
+
+builder.Services.AddIdentity<AppUser, AppRole>()
+	.AddEntityFrameworkStores<Context>()
+	.AddDefaultTokenProviders();
+
+// Add MVC support (with controllers and views)
+builder.Services.AddControllersWithViews();
+
+// Add Authorization Policy for authenticated users
+builder.Services.AddMvc(options =>
+{
+	var policy = new AuthorizationPolicyBuilder()
+		.RequireAuthenticatedUser()
+		.Build();
+	options.Filters.Add(new AuthorizeFilter(policy));
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseAuthentication();
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Default}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Login}/{action=SignIn}/{id?}");
 
 app.Run();
