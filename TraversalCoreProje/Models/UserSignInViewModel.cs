0db92ee8e4a792e216dcb763f8515a504f6d7adf
selimmelih <selimmelih@outlook.com>
2025-01-20 00:37:19 +0300

    Authentication identity login process is ok.

---
+++ TraversalCoreProje/Models/UserSignInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProje.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Kullanıcı Adınızı Giriniz.")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "Lütfen Parolanızı Giriniz.")]
+        public string password { get; set; }
+
+    }
+}
