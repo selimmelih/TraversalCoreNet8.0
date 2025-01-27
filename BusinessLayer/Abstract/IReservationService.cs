f945c81e4e98a95447d12299d52320b3d6faa485
selimmelih <selimmelih@outlook.com>
2025-01-27 23:34:11 +0300

    user id ye gore onay bekleyen rezervasyonlar sayfasi yapildi.

--- BusinessLayer/Abstract/IReservationService.cs
+++ BusinessLayer/Abstract/IReservationService.cs
@@ -9,5 +9,7 @@ namespace BusinessLayer.Abstract
 {
     public interface IReservationService:IGenericService<Reservation>
     {
+        List<Reservation> GetListApprovalReservation(int id);
+
     }
 }
--- TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -13,6 +14,14 @@ namespace TraversalCoreProje.Areas.Member.Controllers
     {
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         ReservationManager reservationManager = new ReservationManager(new EFReservationDal());
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public ReservationController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult MyCurrentReservation()
         {
             var values = reservationManager.TGetList();
@@ -21,6 +30,13 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         public IActionResult MyOldReservation()
         {
             return View();
+        }
+        public async Task<IActionResult> MyApprovalReservation()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var valuesList = reservationManager.GetListApprovalReservation(values.Id);
+            return View(valuesList);
         }
         public IActionResult NewReservation()
         {
