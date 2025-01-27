ea0d2582c31ce376790768f46514e8a3a14c66b8
selimmelih <selimmelih@outlook.com>
2025-01-27 23:12:54 +0300

    rezervasyon sayfalari ve mimarisi ayarlandi

---
+++ TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [Route("Member/[controller]/[action]")]//yönlendirmeyi yaptık calisti güzel bu kod
+
+    public class ReservationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        ReservationManager reservationManager = new ReservationManager(new EFReservationDal());
+        public IActionResult MyCurrentReservation()
+        {
+            var values = reservationManager.TGetList();
+            return View();
+        }
+        public IActionResult MyOldReservation()
+        {
+            return View();
+        }
+        public IActionResult NewReservation()
+        {
+            List<SelectListItem> values = (from x in destinationManager.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value = x.DestinationID.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewReservation(Reservation p)
+        {
+            p.AppUserId = 4;
+            p.Status = "Onay Bekliyor";
+            reservationManager.TAdd(p);
+            return RedirectToAction("MyCurrentReservation");
+        }
+    }
+}
