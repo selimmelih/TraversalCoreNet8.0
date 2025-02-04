93acb504fb9b0bf4f392c27c5d8000b41323f229
selimmelih <selimmelih@outlook.com>
2025-02-04 22:18:45 +0300

    dashboard sayfasinda profil bilgileri kismi duzenlendi, tur rehberlerimiz(guide) kismi eklendi

--- EntityLayer/Concrete/Guide.cs
+++ EntityLayer/Concrete/Guide.cs
@@ -12,11 +12,11 @@ namespace EntityLayer.Concrete
         [Key]
 
         public int GuideID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Image { get; set; }
-        public string TwitterUrl { get; set; }
-        public string InstagramUrl { get; set; }
-        public bool Status { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public string? TwitterUrl { get; set; }
+        public string? InstagramUrl { get; set; }
+        public bool? Status { get; set; }
     }
 }
