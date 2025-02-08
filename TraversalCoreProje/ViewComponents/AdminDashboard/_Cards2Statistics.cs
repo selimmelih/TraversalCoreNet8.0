bc0093543cc37c38fc3ba4f106f3aa17739e850e
selimmelih <selimmelih@outlook.com>
2025-02-09 02:07:55 +0300

    admin dashboard sayfasi tamamlandi viewcomponent yapisi kullanildi

---
+++ TraversalCoreProje/ViewComponents/AdminDashboard/_Cards2Statistics.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.AdminDashboard
+{
+    public class _Cards2Statistics : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProje/ViewComponents/AdminDashboard/_DestinationStatistics2.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.AdminDashboard
+{
+    public class _DestinationStatistics2 : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
