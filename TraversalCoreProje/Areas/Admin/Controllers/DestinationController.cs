1297f05ee7d9cf00a4433ca13f0aff556b11fd6f
selimmelih <selimmelih@outlook.com>
2025-02-09 01:38:40 +0300

    admin destination islemleri tamamlandi CRUD

--- TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
@@ -1,13 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+        public IActionResult AddDestination()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteDestination(int id)
+        {
+            var values = destinationManager.TGetByID(id);
+            destinationManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UpdateDestination(int id)
+        {
+            var values = destinationManager.TGetByID(id);
+
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            destinationManager.TUpdate(destination);
+
+            return RedirectToAction("Index");
+        }
+
+
+
     }
 }
