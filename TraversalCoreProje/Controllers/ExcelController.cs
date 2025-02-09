a51eb70c58cdb44e84fb05d6d27799f456d0415e
selimmelih <selimmelih@outlook.com>
2025-02-09 21:01:26 +0300

    pdf-excel raporlama sayfasi eklendi.

---
+++ TraversalCoreProje/Controllers/ExcelController.cs
@@ -0,0 +1,94 @@
+﻿using ClosedXML.Excel;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class ExcelController : Controller
+    {
+        private readonly Context _context;
+
+        public ExcelController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public List<DestinationModel> DestinationList()
+        {
+            List<DestinationModel> destinationModels = _context.Destinations
+                .Select(x => new DestinationModel
+                {
+                    City = x.City,
+                    DayNight = x.DayNight,
+                    Price = x.Price,
+                    Capacity = x.Capacity
+                })
+                .ToList();
+
+            return destinationModels;
+        }
+
+        public IActionResult StaticExcelReport()
+        {
+
+            ExcelPackage excel = new ExcelPackage();
+
+            var worksheet = excel.Workbook.Worksheets.Add("Sayfa1");
+            worksheet.Cells[1, 1].Value = "Rota";
+            worksheet.Cells[1, 2].Value = "Rehber";
+            worksheet.Cells[1, 3].Value = "Kontenjan";
+
+
+            worksheet.Cells[2, 1].Value = "Gürcistan Batum Turu";
+            worksheet.Cells[2, 2].Value = "Kadir Yıldız";
+            worksheet.Cells[2, 3].Value = "50";
+
+            worksheet.Cells[3, 1].Value = "Sırbistan - Makedonya Turu";
+            worksheet.Cells[3, 2].Value = "Zeynep Öztürk";
+            worksheet.Cells[3, 3].Value = "35";
+
+            var bytes = excel.GetAsByteArray();
+
+            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "dosya2.xlsx");
+        }
+
+        public IActionResult DestinationExcelReport()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var workSheet = workbook.Worksheets.Add("Tur Listesi");
+                workSheet.Cell(1, 1).Value = "Şehir";
+                workSheet.Cell(1, 2).Value = "Konaklama Süresi";
+                workSheet.Cell(1, 3).Value = "Fiyat";
+                workSheet.Cell(1, 4).Value = "Kontenjan";
+
+                int rowCount = 2;
+
+                foreach (var item in DestinationList())
+                {
+                    workSheet.Cell(rowCount, 1).Value = item.City;
+                    workSheet.Cell(rowCount, 2).Value = item.DayNight;
+                    workSheet.Cell(rowCount, 3).Value = item.Price;
+                    workSheet.Cell(rowCount, 4).Value = item.Capacity;
+                    rowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "YeniListe.xlsx");
+            }
+            }
+
+           
+        }
+    }
+}
---
+++ TraversalCoreProje/Controllers/PdfReportController.cs
@@ -0,0 +1,67 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class PdfReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StaticPdfReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pdfreports/" + "dosya1.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+
+            PdfWriter.GetInstance(document, stream);
+            document.Open();
+
+            Paragraph paragraph = new Paragraph("Traversal Rezervasyon Pdf Raporu");
+
+            document.Add(paragraph);
+            document.Close();
+            return File("/pdfreports/dosya1.pdf", "application/pdf", "dosya1.pdf");
+        }
+
+        public IActionResult StaticCustomerReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pdfreports/" + "dosya3.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+
+            PdfWriter.GetInstance(document, stream);
+            document.Open();
+
+
+            PdfPTable pdfPTable = new PdfPTable(3);
+
+            pdfPTable.AddCell("Misafir Adı");
+            pdfPTable.AddCell("Misafir Soyadı");
+            pdfPTable.AddCell("Misafir TC");
+
+
+            pdfPTable.AddCell("Denizğğ");
+            pdfPTable.AddCell("Turhan");
+            pdfPTable.AddCell("110110010");
+
+            pdfPTable.AddCell("Selğim");
+            pdfPTable.AddCell("Turhan");
+            pdfPTable.AddCell("287451578");
+            
+            pdfPTable.AddCell("Deni");
+            pdfPTable.AddCell("Seli");
+            pdfPTable.AddCell("4564987");
+
+            document.Add(pdfPTable);
+
+            document.Close();
+            return File("/pdfreports/dosya3.pdf", "application/pdf", "dosya3.pdf");
+        }
+    }
+}
