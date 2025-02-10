e4f1077c39f52e7fdc2d14a66db38935557d2c56
selimmelih <selimmelih@outlook.com>
2025-02-10 22:38:18 +0300

    city/destination ajax islemleri yapildi. tek sayfada ekleme guncelleme silme goruntuleme scriptleri tanimlandi.

--- TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DocumentFormat.OpenXml.Drawing.Diagrams;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TraversalCoreProje.Models;
 
@@ -7,6 +11,13 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
     [Area("Admin")]
     public class CityController : Controller
     {
+        private readonly IDestinationService _destinationService;
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -14,30 +25,69 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
 
         public IActionResult CityList()
         {
-            var jsonCity = JsonConvert.SerializeObject(cities);
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
             return Json(jsonCity);
         }
 
-        public static List<CityClass> cities = new List<CityClass>
+        [HttpPost]
+        public IActionResult AddCityDestination(Destination destination)
         {
-            new CityClass
+            destination.Status = true;
+            _destinationService.TAdd(destination);
+            var values = JsonConvert.SerializeObject(destination);
+            return Json(values);
+        }
+
+        public IActionResult GetCityById(int DestinationID)
+        {
+            var values = _destinationService.TGetByID(DestinationID);
+            if (values == null)
             {
-                CityID =1,
-                CityName = "Üsküp",
-                CityCountry = "Makedonya"
-            },
-            new CityClass
+
+            }
+            else
             {
-                CityID =2,
-                CityName = "Roma",
-                CityCountry = "İtalya"
-            },
-            new CityClass
-            {
-                CityID =3,
-                CityName = "Londra",
-                CityCountry = "İngiltere"
-            },
-        };
+                var jsonValues = JsonConvert.SerializeObject(values);
+                return Json(jsonValues);
+
+            }
+            return View();
+        }
+        public IActionResult DeleteCity(int DestinationID)
+        {
+            var values = _destinationService.TGetByID(DestinationID);
+            _destinationService.TDelete(values);
+            return NoContent();
+        }
+
+        public IActionResult UpdateCity(Destination destination)
+        {
+            //var values = _destinationService.TGetByID(destination.DestinationID);
+            _destinationService.TUpdate(destination);
+            var v = JsonConvert.SerializeObject(destination);
+            return Json(v);
+        }
+        //statik bir liste örneği
+        //public static List<CityClass> cities = new List<CityClass>
+        //{
+        //    new CityClass
+        //    {
+        //        CityID =1,
+        //        CityName = "Üsküp",
+        //        CityCountry = "Makedonya"
+        //    },
+        //    new CityClass
+        //    {
+        //        CityID =2,
+        //        CityName = "Roma",
+        //        CityCountry = "İtalya"
+        //    },
+        //    new CityClass
+        //    {
+        //        CityID =3,
+        //        CityName = "Londra",
+        //        CityCountry = "İngiltere"
+        //    },
+        //};
     }
 }
