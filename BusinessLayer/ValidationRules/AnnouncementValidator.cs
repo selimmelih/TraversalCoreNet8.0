fb8f2a0a3259848dff28add1ea1381e6779d6b93
selimmelih <selimmelih@outlook.com>
2025-02-12 23:04:57 +0300

    announcement tablosu eklendi ve view sayfasi tamamlandi viewmodel olusturularak

---
+++ BusinessLayer/ValidationRules/AnnouncementValidator.cs
@@ -0,0 +1,25 @@
+﻿using DTOLayer.DTOs.AnnouncementDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AnnouncementValidator:AbstractValidator<AnnouncementAddDTOs>
+    {
+        public AnnouncementValidator()
+        {
+            RuleFor(x=>x.Title).NotEmpty().WithMessage("Lütfen Başlığı Boş Geçmeyiniz");
+            RuleFor(x=>x.Content).NotEmpty().WithMessage("Lütfen İçeriği Boş Geçmeyiniz");
+
+            RuleFor(x => x.Title).MinimumLength(5).WithMessage("Lütfen En Az 5 Karakter Başlık Veri Girişi Yapınız");
+            RuleFor(x => x.Content).MinimumLength(10).WithMessage("Lütfen En Az 10 Karakter İçerik Veri Girişi Yapınız");
+
+            RuleFor(x => x.Title).MaximumLength(50).WithMessage("Lütfen En Fazla 50 Karakter Başlık Veri Girişi Yapınız");
+            RuleFor(x => x.Content).MaximumLength(500).WithMessage("Lütfen En Fazla 500 Karakter İçerik Veri Girişi Yapınız");
+        }
+    }
+}
---
+++ TraversalCoreProje/Areas/Admin/Controllers/AnnouncementController.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProje.Areas.Admin.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AnnouncementController : Controller
+    {
+        private readonly IAnnouncementService _announcementService;
+
+        public AnnouncementController(IAnnouncementService announcementService)
+        {
+            _announcementService = announcementService;
+        }
+
+        public IActionResult Index()
+        {
+            List<Announcement> announcements = _announcementService.TGetList();
+            List<AnnouncementListViewModel> model = new List<AnnouncementListViewModel>();
+            foreach(var item in announcements)
+            {
+                AnnouncementListViewModel announcementListViewModel = new AnnouncementListViewModel();
+                announcementListViewModel.ID = item.AnnouncementID;
+                announcementListViewModel.Title = item.Title;
+                announcementListViewModel.Content = item.Content;
+
+                model.Add(announcementListViewModel);
+            }
+            return View(model);
+        }
+
+        public IActionResult AddAnnouncement()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddAnnouncement(string x)
+        {
+            return View();
+        }
+    }
+}
