159cedea6e67369ac5efaf8b8c3c933ea988fbd4
selimmelih <selimmelih@outlook.com>
2025-02-12 23:55:25 +0300

    announcement list view modelden dto ya tasindi.

---
+++ TraversalCoreProje/Mapping/AutoMapperProfile/MapProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using DTOLayer.DTOs.AnnouncementDTOs;
+using DTOLayer.DTOs.AppUserDTOs;
+using DTOLayer.DTOs.CityDTOs;
+using EntityLayer.Concrete;
+
+namespace TraversalCoreProje.Mapping.AutoMapperProfile
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<AnnouncementAddDto, Announcement>().ReverseMap();
+
+            CreateMap<AnnouncementListDto, Announcement>().ReverseMap();
+
+            CreateMap<AppUserRegisterDTO, AppUser>().ReverseMap();
+
+            CreateMap<AppUserLoginDTO, AppUser>().ReverseMap();
+        }
+    }
+}
