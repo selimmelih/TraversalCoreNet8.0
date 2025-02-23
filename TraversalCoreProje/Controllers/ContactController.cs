7cd6a2e6d8c4971c2b6458667df5a2b178256209
selimmelih <selimmelih@outlook.com>
2025-02-23 23:25:11 +0300

    contact sayfasi icin gerekli controller

---
+++ TraversalCoreProje/Controllers/ContactController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using DTOLayer.DTOs.ContactDTOs;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    [AllowAnonymous]
+    public class ContactController : Controller
+    {
+        private readonly IContactUsService _contactUsService;
+        private readonly IMapper _mapper;
+
+        public ContactController(IContactUsService contactUsService, IMapper mapper)
+        {
+            _contactUsService = contactUsService;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SendMessageDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                _contactUsService.TAdd(new ContactUs()
+                {
+                    MessageDate = DateTime.Now,
+                    MessageBody = model.MessageBody,
+                    Mail = model.Mail,
+                    MessageStatus = true,
+                    Name = model.Name,
+                    Subject = model.Subject
+                });
+                return RedirectToAction("Index","Default");
+            }
+            return View(model);
+        }
+    }
+}
