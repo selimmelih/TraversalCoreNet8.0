5e6fafb65db14e53d1cb77f713628795dbc1dbda
selimmelih <selimmelih@outlook.com>
2025-03-02 20:08:10 +0300

    approle/aspnetroles rol listesi eklendi ve rol ekleme islemi gerceklestirildi

---
+++ TraversalCoreProje/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,51 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProje.Areas.Admin.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Role")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+
+        [Route("AddRole")]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+
+        [Route("AddRole")]
+        [HttpPost]
+        public async Task<IActionResult> AddRole(AddRoleViewModel addRoleViewModel)
+        {
+            AppRole role = new AppRole()
+            {
+                Name = addRoleViewModel.RoleName
+            };
+            var result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Role");
+            }
+            else
+            {
+                return View(result);
+            }
+        }
+    }
+}
