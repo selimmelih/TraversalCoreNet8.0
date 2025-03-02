2419cad7be60fc5733be53edcc005f9006d631c5
selimmelih <selimmelih@outlook.com>
2025-03-02 20:26:26 +0300

    rol silme guncelleme islemleri tamamlandi

--- TraversalCoreProje/Areas/Admin/Controllers/RoleController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/RoleController.cs
@@ -1,4 +1,5 @@
-﻿using EntityLayer.Concrete;
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TraversalCoreProje.Areas.Admin.Models;
@@ -40,12 +41,44 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
             var result = await _roleManager.CreateAsync(role);
             if (result.Succeeded)
             {
-                return RedirectToAction("Index", "Role");
+                return RedirectToAction("Index");
             }
             else
             {
                 return View(result);
             }
+        }
+        [Route("DeleteRole/{id}")]
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            await _roleManager.DeleteAsync(value);
+            return RedirectToAction("Index");
+        }
+
+        [Route("UpdateRole/{id}")]
+        public IActionResult UpdateRole(int id)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            UpdateRoleViewModel updateRoleViewModel = new UpdateRoleViewModel
+            {
+                RoleId = value.Id,
+                RoleName = value.Name
+            };
+            return View(updateRoleViewModel);
+        }
+
+        [HttpPost]
+        [Route("UpdateRole/{id}")]
+        public async Task<IActionResult> UpdateRole(UpdateRoleViewModel updateRoleViewModel)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == updateRoleViewModel.RoleId);
+
+            value.Name = updateRoleViewModel.RoleName;
+
+            await _roleManager.UpdateAsync(value);
+
+            return RedirectToAction("Index");
         }
     }
 }
